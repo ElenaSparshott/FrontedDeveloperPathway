89cc6aa4cbb73cf7a8d28fb390c47a4359937700
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-10-11 09:48:11 +0100

    cards array

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,8 +11,13 @@ let cardsEl = document.getElementById("cards-el")
 
 
 function startGame() {
+    renderGame()
+}
+
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum;
-    cardsEl.textContent = "Cards: " + firstCard + " / " + secondCard;
+    cardsEl.textContent = "Cards: " + cards[0] + " / " + cards[1];
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -22,4 +28,13 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 1
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
