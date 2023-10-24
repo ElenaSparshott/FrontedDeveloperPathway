441e3a739f2472f3df82887339b73f7fff615090
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-10-24 19:53:01 +0100

    styling

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -6,9 +6,22 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+                listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
