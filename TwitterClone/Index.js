216c1dedf4704b4810b7c2efd1e10df131814f48
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-01-14 20:29:21 +0000

    data

--- TwitterClone/Index.js
+++ TwitterClone/Index.js
@@ -1,11 +1,14 @@
 import  { tweetsData } from "./data.js";
-
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
-
 tweetBtn.addEventListener('click', function(){
     console.log(tweetBtn.value)
+})
+
+document.addEventListener('click', function(e){
+    console.log("like", e.target.dataset.like)
+    console.log("retweet", e.target.dataset.retweet)
 })
 
 function getFeedHtml(){
@@ -21,12 +24,22 @@ function getFeedHtml(){
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
                             ${tweet.replies.length}
+
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-heart"
+                            data-like="${tweet.uuid}"
+                            ></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"
+                            data-retweet="${tweet.uuid}"
+                            "></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
