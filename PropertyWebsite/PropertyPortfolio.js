cbf3c85f302c33dd28a241fb669e920d4bb4192b
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-05-10 22:16:53 +0100

    property map

---
+++ PropertyWebsite/PropertyPortfolio.js
@@ -0,0 +1,20 @@
+import {placeholderPropertyObj, propertyForSaleArr} from properties/propertyForSaleArr.js
+
+
+function getPropertyHtml(propertyForSaleArr) 
+
+propertyForSaleArr.map(function()) {
+
+<section class="card">
+    <img src="/images/image"></img>
+    <div class="card-right">
+        <h2>propertyForSaleArr.properitylocation</h2>
+        <h3>propertyForSaleArr.priceGBP</h3>
+        <p>propertyForSaleArr.commet</p>
+        <h3>propertyForSaleArr.roomsM2</h3>
+    </div>
+</section> 
+}
+
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+document.getElementById('container', propertyForSaleArr = placeholderPropertyObj).innerHTML = getPropertyHtml()
